37d9ab4e890ee7dbc5ccf06fd6713b0326b323cb
AdamJDuggan <adamduggan17@gmail.com>
2023-10-26 14:25:49 +0100

    refactor of node.yml file

--- server/build/src/services/store.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const redis_1 = require("redis");
-const sessionStore = (0, redis_1.createClient)();
-sessionStore.on("error", (err) => console.log("Redis Client Error", err));
-const connectStore = async () => await sessionStore.connect();
-connectStore();
-module.exports = sessionStore;

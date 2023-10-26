0e36d093a757ab95e18150485755e1571f30fabe
AdamJDuggan <adamduggan17@gmail.com>
2023-10-26 16:04:57 +0100

    commented out all but one test

--- server/src/tests/goals.test.ts
+++ server/src/tests/goals.test.ts
@@ -35,22 +35,22 @@ describe("Goals tests...", () => {
     });
   });
 
-  describe("Test POST /goals", () => {
-    test("It should respond with 201 created", async () => {
-      const res = await request(app)
-        .post("/api/goals")
-        .send({ text: "Goal One!" });
-      expect(res.statusCode).toBe(201);
-      expect(res.body.message).toBe("Goal created");
-      await deleteGoal(res.body.id);
-    });
-    test("It should catch missing required properties", async () => {
-      const res = await request(app).post("/api/goals").send({});
-      expect(res.statusCode).toBe(400);
-      console.log("RES!", res);
-      expect(res.body.message).toBe("Please add a text field");
-    });
-  });
+  // describe("Test POST /goals", () => {
+  //   test("It should respond with 201 created", async () => {
+  //     const res = await request(app)
+  //       .post("/api/goals")
+  //       .send({ text: "Goal One!" });
+  //     expect(res.statusCode).toBe(201);
+  //     expect(res.body.message).toBe("Goal created");
+  //     await deleteGoal(res.body.id);
+  //   });
+  //   test("It should catch missing required properties", async () => {
+  //     const res = await request(app).post("/api/goals").send({});
+  //     expect(res.statusCode).toBe(400);
+  //     console.log("RES!", res);
+  //     expect(res.body.message).toBe("Please add a text field");
+  //   });
+  // });
 
   // describe("Test POST /goals", () => {
   //   test("It should respond with 201 created", async () => {
@@ -67,21 +67,21 @@ describe("Goals tests...", () => {
   //   });
   // });
 
-  describe("Test PUT /goals", () => {
-    test("It should respond with 200 updated", async () => {
-      const goalToUpdate = await createGoal();
-      await request(app)
-        .put(`/api/goals/${goalToUpdate._id}`)
-        .send({ text: "Goal One updated" })
-        .expect(200);
-      await deleteGoal(goalToUpdate.id);
-    });
-  });
+  // describe("Test PUT /goals", () => {
+  //   test("It should respond with 200 updated", async () => {
+  //     const goalToUpdate = await createGoal();
+  //     await request(app)
+  //       .put(`/api/goals/${goalToUpdate._id}`)
+  //       .send({ text: "Goal One updated" })
+  //       .expect(200);
+  //     await deleteGoal(goalToUpdate.id);
+  //   });
+  // });
 
-  describe("Test DELETE /goals", () => {
-    test("It should respond with 200", async () => {
-      const goalToDelete = await createGoal();
-      await request(app).delete(`/api/goals/${goalToDelete._id}`).expect(200);
-    });
-  });
+  // describe("Test DELETE /goals", () => {
+  //   test("It should respond with 200", async () => {
+  //     const goalToDelete = await createGoal();
+  //     await request(app).delete(`/api/goals/${goalToDelete._id}`).expect(200);
+  //   });
+  // });
 });
